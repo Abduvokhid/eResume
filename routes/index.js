62122e8a1e022776049615e8d44773ef36a62a82
Abduvokhid <Abduvohid1996@inbox.ru>
2021-11-18 17:10:05 +0500

    Started working on views and dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-router.use((req, res) => res.status(404).send("Sorry can't find that!"))
+router.use('/', require('./dashboard'))
+router.use((req, res) => res.status(404).send('Sorry can\'t find that!'))
 
 module.exports = router
