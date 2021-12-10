3c0afd9703bbd007c52db5662292dfaa866c4707
Abduvokhid <Abduvohid1996@inbox.ru>
2021-12-10 17:22:36 +0500

    List of resumes has been added. Started working on edit resume page

--- DAL/resume.js
+++ DAL/resume.js
@@ -1,5 +1,19 @@
 const Resume = require('../models/Resume')
 
-module.exports.getUserResumes = async (user_id) => {
+module.exports.getResumes = async (user_id) => {
   return Resume.find({ user: user_id }).lean()
+}
+
+module.exports.createResume = async (user_id, title, name, about) => {
+  const resume = new Resume({
+    user: user_id,
+    title: title,
+    name: name,
+    about: about
+  })
+  await resume.save()
+}
+
+module.exports.getResumeByID = async (resume_id) => {
+  return Resume.findById(resume_id)
 }
--- routes/resume.js
+++ routes/resume.js
@@ -1,19 +1,19 @@
 const express = require('express')
-const { getUserResumes } = require('../DAL/resume')
+const ResumeDAL = require('../DAL/resume')
 const checkPermission = require('../middlewares/checkPermission')
 const router = express.Router()
 
 router.get('/', checkPermission(), async (req, res) => {
-  const user_resumes = await getUserResumes(req.user._id)
-  const resumes = {
-    public: [],
-    private: [],
-    draft: [],
-  }
-  for (const resume of user_resumes) {
-    resumes[resume.status].push(resume)
-  }
-  res.render('resume/index', { resumes })
+  const user_resumes = await ResumeDAL.getResumes(req.user._id)
+  // const resumes = {
+  //   public: [],
+  //   private: [],
+  //   draft: [],
+  // }
+  // for (const resume of user_resumes) {
+  //   resumes[resume.status].push(resume)
+  // }
+  res.render('resume/index', { user_resumes })
 })
 
 router.get('/new', checkPermission(), async (req, res) => {
@@ -21,7 +21,16 @@ router.get('/new', checkPermission(), async (req, res) => {
 })
 
 router.post('/new', checkPermission(), async (req, res) => {
-  console.log(req.body)
+  const { title, name, about } = req.body
+  await ResumeDAL.createResume(req.user._id, title, name, about)
+  res.redirect('/resume')
+})
+
+router.get('/:id/edit', checkPermission(), async (req, res) => {
+  const resume = await ResumeDAL.getResumeByID(req.params.id)
+  if (!resume) return res.status(404).render('core/not_found', { layout: 'core_layout' })
+  if (resume.user.toString() !== req.user._id.toString()) return res.status(404).render('core/not_found', { layout: 'core_layout' })
+  res.render('resume/edit', { resume })
 })
 
 module.exports = router
